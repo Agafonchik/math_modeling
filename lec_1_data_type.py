5a4cbe346a95c818dce47751d8df102d752a4276
Agafonchik <114502742+Agafonchik@users.noreply.github.com>
2022-10-04 16:43:08 +0000

    Первая лаборато

---
+++ lec_1_data_type.py
@@ -0,0 +1,21 @@
+type
+
+type(3)
+
+print (type(3))
+
+print (type(3.4))
+
+print (type('Молодец!'))
+
+a = 'молодец'
+print(type(a))
+
+a = ['molodec', 1, 'rehnj', 6, 8]
+print (type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))

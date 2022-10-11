5d4562a17717dbaa79eba8fb3e1048b18f3ca054
Agafonchik <114502742+Agafonchik@users.noreply.github.com>
2022-10-11 16:47:43 +0000

    hghrhgurhu

---
+++ lec_2_bool.py
@@ -0,0 +1,15 @@
+print(bool(2))
+
+print(bool(3 > 2))
+
+print(bool("Good"))
+
+print(bool([1, 4, 5]))
+
+print(bool(0))
+
+print(bool(''))
+
+print(bool([]))
+
+print(bool([[]])) # если в списке что то есть, то норм. Лишь бы что то было
---
+++ lec_2_if.py
@@ -0,0 +1,12 @@
+if 1: # принципиально, чтобы была "1"
+  print('Hello 0')
+
+a = 3
+
+if a > 1 :
+  print(f'hello {a}')
+
+b = 5
+if b == 5 : #операция сравнения
+  print(f'hello {b}')
+

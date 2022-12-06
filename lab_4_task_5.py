2061d075aadb5b812d201e1426091353e244e902
Agafonchik <114502742+Agafonchik@users.noreply.github.com>
2022-12-06 14:51:33 +0000

    лекция 4

--- lab_4_task_5.py
+++ lab_4_task_5.py
@@ -3,8 +3,21 @@
 
 import numpy as np
 
-def ty():
+def ty(a, b, h, R):
     P = a * b
     T = 0.5 * a * h
     K = np.pi * R ** 2
-    return a, b, h, R**2
+    print(P, T, K)
+    return a * b, 0.5 * a * h, np.pi * R ** 2
+ty(2, 3, 5, 6)
+
+# Правильный пример работы
+def area(*arg, **kw):
+    if kw['figure'] == 'square':
+        s = arg[0] ** 2
+    elif kw['figure'] == 'rectangle':
+        s = arg[0] * arg[1]
+    else:
+        s = 1 / 2 * arg[0] * arg[1]
+        print(s)
+area(2, 3, 5, 8, figure='square')
